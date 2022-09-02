714ed334a2e352320c56b45c124daf208cfa08fb
Oleksandr Antonenko <ishvarko@gmail.com>
2022-09-02 23:43:36 +0200

    transion commit

--- 08-forms-fetch-api-part-2/1-product-form-v1/index.js
+++ 08-forms-fetch-api-part-2/1-product-form-v1/index.js
@@ -5,11 +5,167 @@ const IMGUR_CLIENT_ID = '28aaa2e823b03b1';
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
 export default class ProductForm {
-  constructor (productId) {
+  defaultProductData = {
+    title: "",
+    description: "",
+    quantity: 1,
+    subcategory: "",
+    status: 1,
+    price: 100,
+    discount: 0,
+  };
+  constructor(productId) {
     this.productId = productId;
   }
 
-  async render () {
+  async render() {
+    const wrapper = document.createElement("div");
+    wrapper.innerHTML = this.template;
+    const element = wrapper.firstElementChild;
+    this.element = element;
+    this.subElements = this.getSubElements(element);
+    const categoriesPromise = this.getCategories();
+    const productPromise = this.productId
+      ? this.getProductData(this.productId)
+      : Promise.resolve(this.defaultProductData);
+    const [categories, productData] = await Promise.all([
+      categoriesPromise,
+      productPromise,
+    ]);
+    this.productData = productData;
+    console.log(this.productData);
+    this.insertCategories(categories);
+    this.insertProductData();
+  }
+  insertProductData() {
+    this.subElements.productForm.title.value = this.productData.title;
+    this.subElements.productForm.description.value =
+      this.productData.description;
+    this.subElements.productForm.price.value = this.productData.price;
+    this.subElements.productForm.discount.value = this.productData.discount;
+    this.subElements.productForm.quantity.value = this.productData.quantity;
+    this.subElements.productForm.status.value = this.productData.status;
+    const imageList = this.subElements.imageListContainer.firstElementChild;
 
+    if (
+      Array.isArray(this.productData.images) &&
+      this.productData.images.length
+    ) {
+      console.log(this.productData.images);
+      const html = this.productData.images
+        .map((image) => {
+          return /*html*/ `
+                <li class="products-edit__imagelist-item sortable-list__item" style="">
+              <input type="hidden" name="url" value="${image.url}">
+              <input type="hidden" name="source" value="${image.source}">
+              <span>
+            <img src="icon-grab.svg" data-grab-handle="" alt="grab">
+            <img class="sortable-table__cell-img" alt="Image" src="${image.url}">
+            <span>${image.source}</span>
+          </span>
+              <button type="button">
+                <img src="icon-trash.svg" data-delete-handle="" alt="delete">
+              </button></li>`;
+        })
+        .join("");
+      imageList.innerHTML = html;
+    }
+  }
+  insertCategories(categories) {
+    console.log(this.subElements.productForm);
+    categories.forEach((cat) => {
+      cat.subcategories.forEach((subCat) => {
+        const selected =
+          this.productData.subcategory === subCat.id ? true : false;
+        const option = new Option(
+          `${cat.title} > ${subCat.title}`,
+          subCat.id,
+          false,
+          selected
+        );
+        this.subElements.productForm.subcategory.append(option);
+      });
+    });
+  }
+
+  async getCategories() {
+    const queryUrl = "/api/rest/categories";
+    const url = new URL(queryUrl, BACKEND_URL);
+    url.searchParams.set("_sort", "weight");
+    url.searchParams.set("_refs", "subcategory");
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+  }
+  async getProductData(productId) {
+    const queryUrl = "/api/rest/products";
+    const url = new URL(queryUrl, BACKEND_URL);
+    url.searchParams.set("id", productId);
+    const res = await fetch(url);
+    const data = await res.json();
+    return data[0];
+  }
+  get template() {
+    return /*html*/ `
+        <div class="product-form">
+          <form data-element="productForm" class="form-grid">
+            <div class="form-group form-group__half_left">
+              <fieldset>
+                <label class="form-label">Название товара</label>
+                <input required="" type="text" name="title" class="form-control" placeholder="Название товара">
+              </fieldset>
+            </div>
+            <div class="form-group form-group__wide">
+              <label class="form-label">Описание</label>
+              <textarea required="" class="form-control" name="description" data-element="productDescription" placeholder="Описание товара"></textarea>
+            </div>
+            <div class="form-group form-group__wide" data-element="sortable-list-container">
+              <label class="form-label">Фото</label>
+              <div data-element="imageListContainer"><ul class="sortable-list">
+                </ul></div >
+                <button type="button" name="uploadImage" class="button-primary-outline"><span>Загрузить</span></button>
+            </div>
+            <div class="form-group form-group__half_left">
+              <label class="form-label">Категория</label>
+              <select class="form-control" name="subcategory">
+              </select>
+            </div>
+            <div class="form-group form-group__half_left form-group__two-col">
+              <fieldset>
+                <label class="form-label">Цена ($)</label>
+                <input required="" type="number" name="price" class="form-control" placeholder="100">
+              </fieldset>
+              <fieldset>
+                <label class="form-label">Скидка ($)</label>
+                <input required="" type="number" name="discount" class="form-control" placeholder="0">
+              </fieldset>
+            </div>
+            <div class="form-group form-group__part-half">
+              <label class="form-label">Количество</label>
+              <input required="" type="number" class="form-control" name="quantity" placeholder="1">
+            </div>
+            <div class="form-group form-group__part-half">
+              <label class="form-label">Статус</label>
+              <select class="form-control" name="status">
+                <option value="1">Активен</option>
+                <option value="0">Неактивен</option>
+              </select>
+            </div>
+            <div class="form-buttons">
+              <button type="submit" name="save" class="button-primary-outline">
+                Сохранить товар
+              </button>
+            </div>
+          </form>
+      </div>`;
+  }
+  getSubElements(element) {
+    const result = {};
+    const elements = element.querySelectorAll("[data-element]");
+    for (const subElement of elements) {
+      const name = subElement.dataset.element;
+      result[name] = subElement;
+    }
+    return result;
   }
 }
--- 08-forms-fetch-api-part-2/1-product-form-v1/index.spec.js
+++ 08-forms-fetch-api-part-2/1-product-form-v1/index.spec.js
@@ -4,111 +4,110 @@ import productData from './__mocks__/product-data.js';
 import categoriesData from './__mocks__/categories-data.js';
 
 describe('forms-fetch-api-part-2/product-form-v1', () => {
-  let productFormComponent;
+    let productFormComponent;
 
-  beforeEach(async () => {
-    fetchMock
-      .once(JSON.stringify(categoriesData))
-      .once(JSON.stringify(productData));
+    beforeEach(async() => {
+        fetchMock
+            .once(JSON.stringify(categoriesData))
+            .once(JSON.stringify(productData));
 
-    const productId = 'some-id';
+        const productId = 'some-id';
 
-    productFormComponent = new ProductForm(productId);
+        productFormComponent = new ProductForm(productId);
 
-    const element = await productFormComponent.render();
+        const element = await productFormComponent.render();
 
-    document.body.append(element);
-  });
+        document.body.append(element);
+    });
 
-  afterEach(() => {
-    fetchMock.resetMocks();
-    productFormComponent.destroy();
-    productFormComponent = null;
-  });
+    afterEach(() => {
+        fetchMock.resetMocks();
+        productFormComponent.destroy();
+        productFormComponent = null;
+    });
 
-  it('should be rendered correctly', () => {
-    expect(productFormComponent.element).toBeVisible();
-    expect(productFormComponent.element).toBeInTheDocument();
-  });
+    it('should be rendered correctly', () => {
+        expect(productFormComponent.element).toBeVisible();
+        expect(productFormComponent.element).toBeInTheDocument();
+    });
 
-  it('should render categories data correctly', () => {
-    const subcategory = productFormComponent.element.querySelector('#subcategory');
+    it('should render categories data correctly', () => {
+        const subcategory = productFormComponent.element.querySelector('#subcategory');
 
-    function prepareCategoryName () {
-      const names = [];
+        function prepareCategoryName() {
+            const names = [];
 
-      for (const category of categoriesData) {
-        for (const child of category.subcategories) {
-          names.push(`${category.title} > ${child.title}`);
+            for (const category of categoriesData) {
+                for (const child of category.subcategories) {
+                    names.push(`${category.title} > ${child.title}`);
+                }
+            }
+
+            return names;
         }
-      }
 
-      return names;
-    }
+        const categoriesNames = prepareCategoryName();
 
-    const categoriesNames = prepareCategoryName();
+        expect(subcategory.children[0]).toHaveTextContent(categoriesNames[0]);
+        expect(subcategory.children[subcategory.children.length - 1])
+            .toHaveTextContent(categoriesNames[categoriesNames.length - 1]);
+    });
 
-    expect(subcategory.children[0]).toHaveTextContent(categoriesNames[0]);
-    expect(subcategory.children[subcategory.children.length - 1])
-      .toHaveTextContent(categoriesNames[categoriesNames.length - 1]);
-  });
+    it('should render product data correctly', () => {
+        const { productForm, imageListContainer } = productFormComponent.subElements;
+        const defaultFormData = {
+            title: '',
+            description: '',
+            quantity: 1,
+            subcategory: '',
+            status: 1,
+            price: 100,
+            discount: 0
+        };
 
-  it('should render product data correctly', () => {
-    const { productForm, imageListContainer } = productFormComponent.subElements;
-    const defaultFormData = {
-      title: '',
-      description: '',
-      quantity: 1,
-      subcategory: '',
-      status: 1,
-      price: 100,
-      discount: 0
-    };
+        const fields = Object.keys(defaultFormData);
+        const values = {};
 
-    const fields = Object.keys(defaultFormData);
-    const values = {};
+        for (const field of fields) {
+            values[field] = productForm.querySelector(`#${field}`).value;
+        }
 
-    for (const field of fields) {
-      values[field] = productForm.querySelector(`#${field}`).value;
-    }
+        const imagesHTMLCollection = imageListContainer.querySelectorAll('.sortable-table__cell-img');
 
-    const imagesHTMLCollection = imageListContainer.querySelectorAll('.sortable-table__cell-img');
+        values.images = [];
+        values.id = productFormComponent.productId;
 
-    values.images = [];
-    values.id = productFormComponent.productId;
+        for (const image of imagesHTMLCollection) {
+            values.images.push({
+                url: image.src,
+                source: image.alt
+            });
+        }
 
-    for (const image of imagesHTMLCollection) {
-      values.images.push({
-        url: image.src,
-        source: image.alt
-      });
-    }
+        expect(values.id).toBe('some-id');
+        expect(values.title).toBe('10.1" Планшет Lenovo Tab E10 TB-X104L 32 ГБ 3G, LTE черный');
+        expect(values.quantity).toBe('73');
+        expect(values.price).toBe('10');
+        expect(values.discount).toBe('21');
+        expect(values.images[0].url).toBe('https://shop-image.js.cx/101-planset-lenovo-tab-e10-tb-x104l-32-gb-3g-lte-cernyj-8.jpg');
+    });
 
-    expect(values.id).toBe('some-id');
-    expect(values.title).toBe('10.1" Планшет Lenovo Tab E10 TB-X104L 32 ГБ 3G, LTE черный');
-    expect(values.quantity).toBe('73');
-    expect(values.price).toBe('10');
-    expect(values.discount).toBe('21');
-    expect(values.images[0].url).toBe('https://shop-image.js.cx/101-planset-lenovo-tab-e10-tb-x104l-32-gb-3g-lte-cernyj-8.jpg');
-  });
+    it('should dispatch "product-updated" event after product creating', async() => {
+        const spyDispatchEvent = jest.spyOn(productFormComponent.element, 'dispatchEvent');
 
-  it('should dispatch "product-updated" event after product creating', async () => {
-    const spyDispatchEvent = jest.spyOn(productFormComponent.element, 'dispatchEvent');
+        fetchMock
+            .once(JSON.stringify({ status: 'ok' }));
 
-    fetchMock
-      .once(JSON.stringify({status: 'ok'}));
+        await productFormComponent.save();
 
-    await productFormComponent.save();
+        const [event] = spyDispatchEvent.mock.calls;
 
-    const [event] = spyDispatchEvent.mock.calls;
+        expect(event[0].type).toEqual('product-updated');
+    });
 
-    expect(event[0].type).toEqual('product-updated');
-  });
+    it('should have ability to be removed', () => {
+        productFormComponent.remove();
 
-  it('should have ability to be removed', () => {
-    productFormComponent.remove();
-
-    expect(productFormComponent.element).not.toBeInTheDocument();
-  });
+        expect(productFormComponent.element).not.toBeInTheDocument();
+    });
 });
-
